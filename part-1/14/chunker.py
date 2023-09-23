a6cfd174531bb549a919912ba69052abec79cd53
Abel <aa1650694369@gmail.com>
2023-09-24 00:23:40 +0800

    Implement my_enumerate and chunker generator functions
    Two new Python scripts have been added: `my_enumerate.py` and `chunker.py`. In `my_enumerate.py`, a generator function my_enumerate is implemented to mimic Python's built-in enumerate function. The `chunker.py` introduces a function that splits an iterable into chunks of a given size. Both these scripts are useful for case scenarios where we need to manipulate and produce data in chunks or need index while iterating data.

---
+++ part-1/14/chunker.py
@@ -0,0 +1,8 @@
+def chunker(iterable, size):
+    # Implement function here
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk))
